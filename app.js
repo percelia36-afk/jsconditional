cda79e7ea87d899122a53da8009dc737dc62e06e
percelia36-afk <percelia36@gmail.com>
2025-09-04 14:57:00 +0100

    some of the workshop

---
+++ app.js
@@ -0,0 +1,12 @@
+if (1 > 0) {
+  console.log("1 is greater than 0");
+}
+
+// assume you're not old enough to drive
+let isRoadLegal = false; // boolean does change after checks
+const age = 18; // const: age doesn't change during the running program
+const legalDrivingAge = 17;
+
+if (age > legalDrivingAge) {
+  isRoadLegal = true;
+}
